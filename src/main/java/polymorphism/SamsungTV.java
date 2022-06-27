80d7ec7de020072ad302a6babc95c9c64df6bb0a
WonJun Yeo <rbgh004@gmail.com>
2022-06-27 16:06:15 +0900

    Dependency Injection practice

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -1,40 +1,29 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
-
-//	public void powerOn() {
-//		System.out.println("SamsungTv---전원 켠다.");
-//	}
-//	public void powerOff() {
-//		System.out.println("SamsungTv---전원 끈다.");
-//	}
-//	public void volumeUp() {
-//		System.out.println("SamsungTv---소리를 올린다.");
-//	}
-//	public void volumeDown() {
-//		System.out.println("SamsungTv---소리를 내린다.");
-//	}
-	
-	// 다형성을 이용한 결합도 낮추기
+	// SnoySpeaker에 의존
+	private Speaker speaker;
+	private int price;
 
 	@Override
 	public void powerOn() {
-		System.out.println("SamsungTv---전원 켠다.");
+		System.out.println("SamsungTv---전원 켠다.(가격 : " + price + ")");
 	}
 
 	@Override
 	public void powerOff() {
 		System.out.println("SamsungTv---전원 끈다.");
 	}
-
+	
+	
 	@Override
 	public void volumeUp() {
-		System.out.println("SamsungTv---소리를 올린다.");
+		speaker.volumeUp();
 	}
 
 	@Override
 	public void volumeDown() {
-		System.out.println("SamsungTv---소리를 내린다.");
+		speaker.volumeDown();
 	}
 	
 	// 기본생성자
@@ -42,13 +31,35 @@ public class SamsungTV implements TV {
 		System.out.println("===> SamsungTV 객체 생성");
 	}
 	
+	public SamsungTV(Speaker spaeker) {
+		System.out.println("===> SamsungTV(2) 객체 생성");
+		this.speaker = spaeker;
+	}
+	
+	public SamsungTV(Speaker spaeker, int price) {
+		System.out.println("===> SamsungTV(3) 객체 생성");
+		this.speaker = spaeker;
+		this.price = price;
+	}
+	
 	// init-method로 초기화할 메서드
 	public void initMethod() {
 		System.out.println("객체 초기화 작업 처리..");
 	}
 	
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===> setSpeaker() 호출");
+		this.speaker = speaker;
+	}
+
+	public void setPrice(int price) {
+		System.out.println("===> setPrice() 호출");
+		this.price = price;
+	}
+
 	// destroy-method로 객체가 삭제되기 전 실행될 메서드
 	public void destroyMethod() {
 		System.out.println("객체 삭제 전 처리할 로직 처리...");
 	}
+
 }
